723ba508328a90595bd3ced5e45e260a5659aee2
MelvimJones <melvimjones32@gmail.com>
2023-05-22 19:03:19 -0400

    revert

--- src/main.js
+++ src/main.js
@@ -1,16 +1,3 @@
-//--------------CODIGO P/ PWA-------------------------
-if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('../public/sw.js')
-  .then(function(registration){
-    console.log("registration successful, scope is:",registration.scope)
-  })
-  .catch(function(error){
-    console.log("service worker registration failed, error: ", error)
-  });
-}
-
-
-//--------------CODIGO NATIVO-------------------------
 import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
